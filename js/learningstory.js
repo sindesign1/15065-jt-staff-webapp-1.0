994809055f84ae190faf3115f4c6ee66781f80cb
chelsmcg <chelsea@sindesign.com.au>
2015-07-07 14:18:51 +1000

    fixing input boxes styling

--- js/learningstory.js
+++ js/learningstory.js
@@ -2,7 +2,6 @@ var Story = {
 
 	init: function() {
 		Story.events();
-		// Story.coverImageOffset();
 	},
 
 	events: function() {
@@ -24,72 +23,10 @@ var Story = {
 		$('#subHeaderInput').val(subHeaderText);
 	},
 
-	// coverImageOffset: function() {
-
-	// 	var offset = $(window).offset();
-	// 	console.log(offset);
-
-	// 	var top = offset.top;
-
-	// 	if(top === 0) {
-	// 		console.log('at the top');
-	// 	} else {
-	// 		console.log('scrolled');
-	// 	}
-
-	// }
-
-	scrollFunction: function() {
-
-		var $coverPageDiv = $('body').find('#coverPageContainer');
-		var $contentDiv = $('body').find('#learningStoryPage');
-
-		if( $(window).scrollTop() > $coverPageDiv.offset().top + 1 ) {
-            $contentDiv.css('display', 'block');
-        } else {
-            $contentDiv.css('display', 'none');
-        }
-
-	}
-
-	// scrollFunction: function() {
-	// 	var lastScrollTop = 0;
-	// 	// var delta = 5;
-
-	// 	var st = $(this).scrollTop();
-
-	// 	// if(Math.abs(lastScrollTop - st) <= delta) {
-	// 	// 	return;
-	// 	// }
-
-	// 	if(st > lastScrollTop) {
-	// 		console.log('scroll down');
-	// 	} else if(st < lastScrollTop) {
-	// 		console.log('scroll up');
-	// 	}
-
-	// 	lastScrollTop = st;
-	// }
+	
 
 }
 
 $(document).ready(function(){
 	Story.init();
 });
-
-
-// var lastScrollTop = 0, delta = 5;
-//     $(window).scroll(function(event){
-//        var st = $(this).scrollTop();
-       
-//        if(Math.abs(lastScrollTop - st) <= delta)
-//           return;
-       
-//        if (st > lastScrollTop){
-//            // downscroll code
-//            console.log('scroll down');
-//        } else {
-//           // upscroll code
-//           console.log('scroll up');
-//        }
-//        lastScrollTop = st;
