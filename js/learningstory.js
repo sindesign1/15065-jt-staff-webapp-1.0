2ea3bac4b20d832ed16cc8216951712a41708c18
chelsmcg <chelsea@sindesign.com.au>
2015-07-08 17:05:38 +1000

    learning story functionality

--- js/learningstory.js
+++ js/learningstory.js
@@ -8,8 +8,11 @@ var Story = {
 
 	events: function() {
 		$('body').on('click touch', '#header', Story.changeHeaderText);
-		$('body').on('click touch', '#addElementBtn', Story.openAddElement);
+		$('body').on('click touch', '.addElementBtn', Story.openAddElement);
 		$('body').on('click touch', '#nevermindBtn', Story.closeAddElement);
+		$('body').on('click touch', '#addPhotoElement', Story.addPhotoElement);
+		$(document).on('click touch','#learningStoryPage', Story.closePhotoSidebar);
+
 	},
 
 	changeHeaderText: function(e) {
@@ -66,6 +69,42 @@ var Story = {
 
 	closeAddElement: function() {
 		$('#addElementContainer').css('display', 'none');
+	},
+
+	addPhotoElement: function() {
+
+		window.divForBackground = '.singleImage';
+
+		$('#addElementContainer').css('display', 'none');
+
+		$('#learningStoryPage').css('height', '100%');
+
+		$('html, body').animate({
+	        scrollTop: $("#learningStoryPage").offset().top
+	    }, 1000);
+
+	    $('#addImageSidebar').addClass('display');
+		if($('#addImageSidebar').hasClass('sidebarLeft')) {
+			$('#addImageSidebar').addClass('openLeft');
+		} else if($('#addImageSidebar').hasClass('sidebarRight')) {
+			$('#addImageSidebar').addClass('openRight');
+		}
+
+		if($('#addImageSidebar').hasClass('openRight')) {
+			$('#learningStoryPage').css('padding', '20px 20px');
+			$('.singleImage').css('width', '56%');
+		}
+
+		$('.singleImage').css('display', 'block');
+	},
+
+	closePhotoSidebar: function() {
+		$('#addImageSidebar').removeClass('display');
+		if($('#addImageSidebar').hasClass('sidebarLeft')) {
+			$('#addImageSidebar').removeClass('openLeft');
+		} else if($('#addImageSidebar').hasClass('sidebarRight')) {
+			$('#addImageSidebar').removeClass('openRight');
+		}
 	}
 
 }
