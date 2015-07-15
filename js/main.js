80d8551bc421fe33bfcef3a584a2fcebd1dfbfb0
chelsmcg <chelsea@sindesign.com.au>
2015-07-16 09:58:11 +1000

    fixed faq page functionality and added jacob thumbnails

--- js/main.js
+++ js/main.js
@@ -26,10 +26,71 @@ var Main = {
                 }
             }
         });
+
+        $('body').on('click touch', '#help1', Main.showHelp1);
+        $('body').on('click touch', '#help2', Main.showHelp2);
+        $('body').on('click touch', '#help3', Main.showHelp3);
+        $('body').on('click touch', '#help4', Main.showHelp4);
+        $('body').on('click touch', '#help5', Main.showHelp5);
+        $('body').on('click touch', '#help6', Main.showHelp6);
     },
 
     getCurrentScroll: function() {
         return window.pageYOffset || document.documentElement.scrollTop;
+    },
+
+    showHelp1: function() {
+        $('#help1Tab').fadeIn();
+        $('#help2Tab').hide();
+        $('#help3Tab').hide();
+        $('#help4Tab').hide();
+        $('#help5Tab').hide();
+        $('#help6Tab').hide();
+    },
+
+    showHelp2: function() {
+        $('#help2Tab').fadeIn();
+        $('#help1Tab').hide();
+        $('#help3Tab').hide();
+        $('#help4Tab').hide();
+        $('#help5Tab').hide();
+        $('#help6Tab').hide();
+    },
+
+    showHelp3: function() {
+        $('#help3Tab').fadeIn();
+        $('#help2Tab').hide();
+        $('#help1Tab').hide();
+        $('#help4Tab').hide();
+        $('#help5Tab').hide();
+        $('#help6Tab').hide();
+    },
+
+    showHelp4: function() {
+        $('#help4Tab').fadeIn();
+        $('#help2Tab').hide();
+        $('#help3Tab').hide();
+        $('#help1Tab').hide();
+        $('#help5Tab').hide();
+        $('#help6Tab').hide();
+    },
+
+    showHelp5: function() {
+        $('#help5Tab').fadeIn();
+        $('#help2Tab').hide();
+        $('#help3Tab').hide();
+        $('#help4Tab').hide();
+        $('#help1Tab').hide();
+        $('#help6Tab').hide();
+    },
+
+    showHelp6: function() {
+        $('#help6Tab').fadeIn();
+        $('#help2Tab').hide();
+        $('#help3Tab').hide();
+        $('#help4Tab').hide();
+        $('#help5Tab').hide();
+        $('#help1Tab').hide();
     },
 }
 
